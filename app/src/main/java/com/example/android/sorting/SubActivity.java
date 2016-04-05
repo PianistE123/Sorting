d97fce187901af946bcaea61dd06ca32519d25f8
Pianist-PC <eomega123@gmail.com>
2016-04-06 01:58:27 +0300

    Beta

--- app/src/main/java/com/example/android/sorting/SubActivity.java
+++ app/src/main/java/com/example/android/sorting/SubActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.sorting;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -15,14 +16,27 @@ public class SubActivity extends AppCompatActivity {
         Intent child = getIntent();
         String art = child.getStringExtra("a");
         String inf = child.getStringExtra("i");
-        int imageId = child.getIntExtra("id",0);
+        int imageId = child.getIntExtra("id",1);
         description(art,inf,imageId);
     }
 
     public void description(String article,String inf,int id){
         TextView art = (TextView)findViewById(R.id.article_text_view);
         TextView information = (TextView)findViewById(R.id.information_text_view);
+        ImageView image = (ImageView)findViewById(R.id.image_view);
         art.setText(article);
         information.setText(inf);
+        if (id == 1) {
+            image.setImageResource(R.drawable.quicksort);
+        }
+        if (id == 2) {
+            image.setImageResource(R.drawable.bubbles);
+        }
+        if (id == 3) {
+            image.setImageResource(R.drawable.chet);
+        }
+        if (id == 4) {
+            image.setImageResource(R.drawable.shake);
+        }
     }
 }
