0e9866193ab36723a9783b0cd2ecad9c5e66cc15
Pianist-PC <eomega123@gmail.com>
2016-04-05 23:58:30 +0300

    Beta

--- app/src/main/java/com/example/android/sorting/SubActivity.java
+++ app/src/main/java/com/example/android/sorting/SubActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sorting;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
